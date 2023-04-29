012a529bcc8493fb14e293450ecf0536d621e9be
Raximboy7 <122911999+Raximboy7@users.noreply.github.com>
2023-04-29 13:02:46 +0500

    soda

---
+++ kalkulator/kalkulator.py
@@ -0,0 +1,19 @@
+a = int(input('1-son kriting:'))
+e = input('operatorni kriting(/,+,-,*):')
+b = int(input('1-son kriting:'))
+
+
+if e == '-':
+    print(a - b)
+    
+if e == '+':
+    print(a + b)
+
+if e == '*':
+    print(a * b)
+
+if e == '/':
+    print(a / b)
+    
+if e == '^':
+    print(a ** b)
